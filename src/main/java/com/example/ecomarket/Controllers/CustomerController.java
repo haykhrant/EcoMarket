4ab5a9cb07babd315c5b11cea3973eb649b537f2
mhayk <mhayk.973779@gmail.com>
2021-08-07 16:31:43 +0400

    CrossOrigin anotation Added

--- src/main/java/com/example/ecomarket/Controllers/CustomerController.java
+++ src/main/java/com/example/ecomarket/Controllers/CustomerController.java
@@ -5,12 +5,10 @@ import com.example.ecomarket.DOM.CustomerResponse;
 import com.example.ecomarket.Services.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://52.15.67.16:8080")
 public class CustomerController {
     public final CustomerService customerService;
 
