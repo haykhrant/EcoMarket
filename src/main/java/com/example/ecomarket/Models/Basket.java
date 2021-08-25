89e46d8540174a8d802bd3fd2a91f3967a7ac6ea
hmuradya <mhayk.973779@gmail.com>
2021-08-26 01:54:28 +0400

    CRUD updated

--- src/main/java/com/example/ecomarket/Models/Basket.java
+++ src/main/java/com/example/ecomarket/Models/Basket.java
@@ -1,14 +1,14 @@
-//package com.example.ecomarket.Models;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity
-//public class Basket {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//}
+package com.example.ecomarket.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
