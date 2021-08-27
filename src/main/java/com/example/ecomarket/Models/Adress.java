f34df67ae4f0d8904d45c35e181318a7790c0979
haykhrant <87911466+haykhrant@users.noreply.github.com>
2021-08-27 23:56:19 +0400

    Update Adress.java

--- src/main/java/com/example/ecomarket/Models/Adress.java
+++ src/main/java/com/example/ecomarket/Models/Adress.java
@@ -3,8 +3,8 @@ package com.example.ecomarket.Models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "adresses")
-public class Adress {
+@Table(name = "addresses")
+public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
