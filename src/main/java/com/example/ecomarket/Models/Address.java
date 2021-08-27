2634d25f006af10aaee6009745d043c3e7326da2
haykhrant <87911466+haykhrant@users.noreply.github.com>
2021-08-28 01:00:23 +0400

    Update Address.java

--- src/main/java/com/example/ecomarket/Models/Address.java
+++ src/main/java/com/example/ecomarket/Models/Address.java
@@ -3,7 +3,7 @@ package com.example.ecomarket.Models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "addresses")
+@Table(name = "adresses")
 public class Address {
 
     @Id
@@ -21,5 +21,60 @@ public class Address {
 
     @Column
     private String house;
+
+    //region Constructors
+    public Address() {
+    }
+
+    public Address(Long id, String region, String city, String street, String house) {
+        this.id = id;
+        this.region = region;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+    }
+    //endregion
+
+    //region Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+    //endregion
 }
 
